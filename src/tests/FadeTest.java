85ab94751d741c07b72c8b7930929bf5a099453c
moles <olesmichalxp@gmail.com>
2016-09-17 18:10:22 +0200

    point test skeleton

---
+++ src/tests/FadeTest.java
@@ -0,0 +1,11 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by moles on 2016-09-17.
+ * @author moles
+ */
+public class FadeTest {
+
+}
---
+++ src/tests/RGBPointTest.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by moles on 2016-09-17.
+ * @author moles
+ */
+public class RGBPointTest {
+    @Test
+    public void negative() throws Exception {
+
+    }
+
+    @Test
+    public void greyScale() throws Exception {
+
+    }
+
+    @Test
+    public void sepia() throws Exception {
+
+    }
+
+    @Test
+    public void fadeColor() throws Exception {
+
+    }
+
+}
